aeba8f9257bb073473a291af106b5a5943fb3e76
Miquéias Castro Soares <107892315+miqueiassoares@users.noreply.github.com>
2023-01-15 09:43:16 -0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 "use strict";
 
-let ligado = true;
-let index = 0;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
-if (ligado) {
-    setInterval(()=> {
-        console.log(index++);
-    }, 1000);
+const iniciarBtn = document.querySelector("#iniciar");
+const exibicao = document.querySelector("#tempo");
+
+
+iniciarBtn.addEventListener("click", start);
+
+function start() {
+    console.log("logou");
+    setInterval(contador(), 1000);
 }
+
+function twoNumbers(numero) {
+    if (numero<10) {
+        return "0"+numero
+    } else {
+        return numero
+    }
+}
+
+function contador() {
+    segundos++;
+    if (segundos === 60) {
+        segundos = 0;
+        minutos++;
+        if (minutos === 60) {
+            minutos = 0;
+            horas++;
+        }
+    }
+
+    console.log(segundos);
+    exibicao.innerText = `${twoNumbers(horas)}:${twoNumbers(minutos)}:${twoNumbers(segundos)}`;
+}
+
+
+console.log();
